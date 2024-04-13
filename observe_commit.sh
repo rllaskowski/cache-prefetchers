967e9cbf3d0ec2783e4d4de96c99a6d5b283eb76
rllaskowski <robert.laskowski0@gmail.com>
2024-04-13 13:29:37 +0200

    Automatyczny commit

---
+++ observe_commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ustaw wiadomość commita
+commit_message="Automatyczny commit"
+
+# Ustaw nazwę gałęzi
+branch_name="main"
+
+# Obserwuj bieżący katalog i podkatalogi
+fswatch -o . | while read f
+do
+  git add -A
+  git commit -m "$commit_message"
+  git push origin $branch_name
+done
+
