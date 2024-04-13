0c14bd705fcd7946e0d9ae5b4b307bd301472605
rllaskowski <robert.laskowski0@gmail.com>
2024-04-13 13:48:11 +0200

    Automatyczny commit

--- observe_commit.sh
+++ observe_commit.sh
@@ -7,9 +7,10 @@ commit_message="Automatyczny commit"
 branch_name="main"
 
 # Obserwuj bieżący katalog i podkatalogi, ignorując experiments.ipynb
-fswatch -o -e "experiments.ipynb" . | while read f
+fswatch -o . | while read f
 do
-  git add -A
+  git add .
+  git reset experiments.ipynb
   git commit -m "$commit_message"
   git push origin $branch_name
   # Dodaj opóźnienie 30 sekund
