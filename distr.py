ddc783ab49eea10f4113958b1fac70a78399b0b2
rllaskowski <robert.laskowski0@gmail.com>
2024-04-14 20:56:31 +0200

    Automatyczny commit

--- distr.py
+++ distr.py
@@ -83,7 +83,7 @@ def get_training_samples(cache_history, access_history, cache_size, history_size
             to_evict = max(found, key=lambda x: last_occ[x])
 
         x = history + list(cache)
-
+        
         samples.append((x, to_evict))
 
     return samples
